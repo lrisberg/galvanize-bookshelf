3f7f5fe3cc1521be9f6f980a9eea794629aaa5e9
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-04-01 16:51:49 -0600

    passing 1st test part 5

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
+const knex = require('../knex');
+const humps = require('humps');
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/favorites', (req, res, next) => {
+  knex('favorites')
+    .join('books', 'books.id', '=', 'favorites.book_id')
+    .where('favorites.user_id', 1)
+    .then(books => {
+      res.send(humps.camelizeKeys(books));
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
-
 const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex');
 const humps = require('humps');
