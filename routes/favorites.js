9abe8f8139eed84d0816b834ee3978aee230c0fd
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-04-05 21:04:28 -0600

    pass all pt 4 tests

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,72 +4,72 @@ const express = require('express');
 const knex = require('../knex');
 const humps = require('humps');
 const router = express.Router();
+var jwt = require('jsonwebtoken');
 
-function checkAuth(req, res) {
-  let cookies = req.cookies;
-  if (!cookies.Authorization) {
+function checkAuth(req, res, next) {
+  if (req.cookies.token) {
+    jwt.verify(req.cookies.token, 'shhhh', function(err, decoded) {
+      if (err) {
+        res.setHeader("Content-Type", "text/plain");
+        res.status(401);
+        res.send('Unauthorized');
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
     res.setHeader("Content-Type", "text/plain");
     res.status(401);
     res.send('Unauthorized');
-    return false;
-  } else {
-    return true;
   }
 }
 
-router.get('/favorites', (req, res, next) => {
-  if (checkAuth(req, res)) {
-    knex('favorites')
-      .join('books', 'books.id', '=', 'favorites.book_id')
-      .where('favorites.user_id', 1)
-      .then(books => {
-        res.send(humps.camelizeKeys(books));
-      });
-  }
+router.get('/favorites', checkAuth, (req, res, next) => {
+  knex('favorites')
+    .join('books', 'books.id', '=', 'favorites.book_id')
+    .where('favorites.user_id', req.user.userId)
+    .then(books => {
+      res.send(humps.camelizeKeys(books));
+    });
 });
 
 
-router.get('/favorites/check', (req, res, next) => {
-  if (checkAuth(req, res)) {
-    let number = req.query.bookId;
-    knex('favorites')
-      .where('book_id', number)
-      .then(favorites => {
-        res.send(favorites.length > 0)
-      });
-  }
+
+router.get('/favorites/check', checkAuth, (req, res, next) => {
+  let number = req.query.bookId;
+  knex('favorites')
+    .where('book_id', number)
+    .where('user_id', req.user.userId)
+    .then(favorites => {
+      res.send(favorites.length > 0)
+    });
 });
 
-router.post('/favorites', (req, res, next) => {
-  if (checkAuth(req, res)) {
-    let body = req.body;
-    knex('favorites')
-      .returning(['id', 'book_id', 'user_id'])
-      .insert({
-        book_id: body.bookId,
-        user_id: 1
-      })
-      .then(book => {
-        res.setHeader("Content-Type", "application/json")
-        res.send(humps.camelizeKeys(book[0]))
-      })
-  }
+router.post('/favorites', checkAuth, (req, res, next) => {
+  let body = req.body;
+  knex('favorites')
+    .returning(['id', 'book_id', 'user_id'])
+    .insert({
+      book_id: body.bookId,
+      user_id: req.user.userId
+    })
+    .then(book => {
+      res.setHeader("Content-Type", "application/json")
+      res.send(humps.camelizeKeys(book[0]))
+    })
 })
 
-router.delete('/favorites', (req, res, next) => {
-  if (checkAuth(req, res)) {
-    knex('favorites')
-      .returning(["book_id", "user_id"])
-      .where('book_id', req.body.bookId)
-      .del()
-      .then(book => {
-        res.setHeader("Content-Type", "application/json")
-        res.send(humps.camelizeKeys(book[0]));
-      })
-  }
+router.delete('/favorites', checkAuth, (req, res, next) => {
+  knex('favorites')
+    .returning(["book_id", "user_id"])
+    .where('book_id', req.body.bookId)
+    .where('user_id', req.user.userId)
+    .del()
+    .then(book => {
+      res.setHeader("Content-Type", "application/json")
+      res.send(humps.camelizeKeys(book[0]));
+    })
 })
-
-
-
 
 module.exports = router;
--- routes/token.js
+++ routes/token.js
@@ -4,12 +4,63 @@ const express = require('express');
 const knex = require('../knex');
 const humps = require('humps');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-as-promised');
 
+
+router.get('/token', (req, res, next) => {
+  if (req.cookies.token) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(true);
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.send(false);
+  }
+});
 
 router.post('/token', (req, res, next) => {
   let credentials = req.body;
-  res.cookie('Authorization', '1')
-  res.end();
+  let email = credentials.email;
+  let password = credentials.password;
+
+  // if (email === '') {
+  //   res.setHeader("Content-Type", "plain/text");
+  //   res.send('Email must not be blank');
+  //   return;
+  // }
+
+  knex('users')
+    .select(['id', 'email', 'first_name', 'last_name', 'hashed_password'])
+    .where('email', email)
+    .then((users) => {
+      if (users.length === 0) {
+        res.setHeader("Content-Type", "plain/text");
+        res.status(400);
+        res.send('Bad email or password');
+        return;
+      }
+      bcrypt.compare(password, users[0].hashed_password)
+        .then(() => {
+          const token = jwt.sign({
+            userId: users[0].id
+          }, 'shhhh');
+          res.cookie('token', token, {
+            httpOnly: true
+          });
+          const user = users[0];
+          delete user.hashed_password;
+          res.send(humps.camelizeKeys(user));
+        }, () => {
+          res.setHeader('Content-Type', 'plain/text');
+          res.status(400);
+          res.send('Bad email or password');
+        });
+    })
+})
+
+router.delete('/token', (req, res, next) => {
+  res.cookie('token', '');
+  res.send(true);
 })
 
 module.exports = router;
